8c9a90dd27b5573b5480575981198458a0a37b89
Victor <viclee02@gmail.com>
2015-03-09 12:02:08 -1000

    adds express server

---
+++ server/controllers/api.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router  = express.Router();
+
+//list all todos
+router.get('/', function (req, res){
+  res.send("Hello API");
+});
+
+//add todos
+
+
+//delete todos
+
+//complete todos
+
+//incomplete todos
+
+
+module.exports = router;
